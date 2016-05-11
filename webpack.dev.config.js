d39370f959fb596672dee82a919ea9eb6374238d
Tris Setiawan <tris.setiawan@yahoo.com>
2016-05-11 16:30:16 +0700

    dev config

---
+++ webpack.dev.config.js
@@ -0,0 +1,61 @@
+/*eslint-disable no-var*/
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var AureliaWebpackPlugin = require('aurelia-webpack-plugin');
+var ProvidePlugin = require('webpack/lib/ProvidePlugin');
+var DefinePlugin = require('webpack/lib/DefinePlugin');
+var pkg = require('./package.json');
+
+var outputFileTemplateSuffix = '-' + pkg.version;
+
+module.exports = {
+  entry: {
+    main: [
+      './src/main'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]' + outputFileTemplateSuffix + '.js',
+    chunkFilename: '[id]' + outputFileTemplateSuffix + '.js'
+  },
+  plugins: [
+    new AureliaWebpackPlugin(),
+    new DefinePlugin({
+      env: {
+        tokenHeader: "'X-Access-Token'",
+        cookieName: "'__moonlay_cookie'",
+        authEndpoint: "'http://authentication-api-dev.mybluemix.net'",
+        workplanEndpoint: "'https://workplan-api-dev.mybluemix.net'",
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Aurelia webpack skeleton - ' + pkg.version,
+      template: 'index.prod.html',
+      filename: 'index.html'
+    }),
+    new ProvidePlugin({
+      Promise: 'bluebird',
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery' // this doesn't expose jQuery property for window, but expose it to every module
+    })
+  ],
+  resolve: {
+    root: [
+      path.resolve('./')
+    ]
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/, query: { presets: ['es2015-loose', 'stage-1'], plugins: ['transform-decorators-legacy'] } },
+      { test: /\.css?$/, loader: 'style!css' },
+      { test: /\.html$/, loader: 'html' },
+      { test: /\.(png|gif|jpg)$/, loader: 'url?limit=8192' },
+      { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff2' },
+      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' }
+    ]
+  }
+};
